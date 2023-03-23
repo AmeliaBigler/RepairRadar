0d329a6f5d721f7c105b8ecb75797bdd2a7c8725
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-03-22 21:00:51 -0500

    Added function to make a user or mechanic

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Mechanic } = require('../../models');
+
+router.post("/", async (req, res) => {
+  if (req.body.mechanic) {
+    const mechanicData = await Mechanic.findOne({
+      where: {
+        username: req.body.username
+      }
+    })
+    if (mechanicData) {
+      return res.status(404).render("signup", { message: "Username is already taken" })
+    }
+    await Mechanic.create(req.body)
+    req.session.save(() => {
+      req.session.username = req.body.username;
+      req.session.isMechanic = true;
+    });
+    return res.status(201).json("Created")
+  }
+  const userData = await User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  if (userData) {
+    return res.status(404).render("signup", { message: "Username is already taken" })
+  }
+  await User.create(req.body)
+  req.session.save(() => {
+    req.session.username = req.body.username;
+    req.session.isMechanic = true;
+  });
+  return res.status(201).json("Created")
+})
 
 router.post('/login', async (req, res) => {
   try {
@@ -24,7 +57,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
