e5ca0e50faf665b6202985be49e15a3c38aaff62
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-03-23 01:38:20 -0500

    Added routes to bids

--- controller/api/bids.js
+++ controller/api/bids.js
@@ -1,10 +1,34 @@
 const bids = require("express").Router()
+const { Ticket, Bids, User, Mechanic } = require("../../models/index.js")
 
+bids.post("/:id", async (req, res) => {
+    const userData = await User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    const user = userData.get({ plain: true })
+    const ticketData = await Ticket.findByPk(req.params.id)
+    const ticket = ticketData.get({ plain: true })
+    await Bids.create({
+        content: req.body.content,
+        mechanicId: user.id,
+        ticketId: ticket.id
+    })
+    res.status(201).json("Create")
+})
 
-
-
-
-
-
+bids.delete("/bid/:id", async (req, res) => {
+    await Bids.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    const ticketData = await Ticket.findByPk(req.body.id, {
+        include: { model: Bids }
+    });
+    const ticket = ticketData.get({ plain: true });
+    res.render("dashboard", { ticket });
+});
 
 module.exports = bids
