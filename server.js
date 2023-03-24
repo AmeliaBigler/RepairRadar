a1392773be4ebdeae6985d30fdf281c3f438cdc2
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-03-23 21:24:23 -0500

    Added semicolons to server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require("express")
-const session = require("express-session")
-const SequelizeStore = require("connect-session-sequelize")(session.Store)
-const sequelize = require("./config/connection")
-const { Ticket } = require("./models/index")
-const exphbs = require("express-handlebars")
-const hbs = exphbs.create({})
-const router = require("./controller/index.js")
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const router = require("./controller/index.js");
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(session({
     secret: 'sdjfaklasdfkljasdfj;k',
@@ -21,18 +20,18 @@ app.use(session({
     store: new SequelizeStore({
         db: sequelize,
     }),
-}))
+}));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-app.engine("handlebars", hbs.engine)
-app.set("view engine", "handlebars")
-app.use(router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(router);
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 sequelize.sync().then(
     app.listen(PORT, () => {
-        console.log(`Listening at http://localhost:${PORT}`)
+        console.log(`Listening at http://localhost:${PORT}`);
     })
-)
+);
