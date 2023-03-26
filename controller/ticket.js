e9169f547c806b1a4883065bf6c185f9294d35dd
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-26 16:31:12 -0500

    add get route and update post route for newTicket.

--- controller/ticket.js
+++ controller/ticket.js
@@ -8,50 +8,64 @@ ticket.get("/:id", async (req, res) => {
     res.render("ticket", { ticket });
 })
 
-ticket.post("/", isAuth, async (req, res) => {
-    const userData = await User.findByPk(req.session.user_id);
-    const user = userData.get({ plain: true });
-    const newTicket = await Ticket.create({
-        title: req.body.title,
-        carMake: req.body.carMake,
-        carModel: req.body.carModel,
-        modelYear: req.body.modelYear,
-        issue: req.body.issue,
-        userId: user.id
-    }, {
-        returning: ["id"]
+ticket.get("/", isAuth, async (req, res) => {
+    res.render("newTicket", { 
+        logged_in: req.session.logged_in,
+        user: req.session.username
     });
-    if (req.body.parts.length){
-        var parts = req.body.parts
-        var partsWithIds = parts.map(async part => {
-            const partData = Parts.findOne({
-                where: {
-                    name: part
-                }
-            });
-            var partplain = partData.get({ plain: true });
-            return {
-                ticketId: newTicket.id,
-                partsId: partplain.id
-            }
+})
+
+ticket.post("/", isAuth, async (req, res) => {
+    try {
+        // const userData = await User.findByPk(req.session.user_id);
+        // const user = userData.get({ plain: true });
+        const newTicket = await Ticket.create({
+            title: req.body.title,
+            carMake: req.body.carMake,
+            carModel: req.body.carModel,
+            modelYear: req.body.modelYear,
+            issue: req.body.issue,
+            userId: req.session.user_id
+        }, 
+        {
+            returning: ["id"]
         });
-        await TicketParts.bulkCreate(partsWithIds);
-        return res.status(201).render(`/tickets/${newTicket.id}`);
-    };
-        var partsData = await Parts.findOne({
-            where: {
-                name: req.body.parts
-            }
-        });
-        var part = partsData.get({ plain: true});
-        await TicketParts.create({
-            ticketId: newTicket.id,
-            partsId: parts.id
-        })
-        res.status(201).render(`/tickets/${newTicket.id}`);
+    // if (req.body.parts.length){
+    //     var parts = req.body.parts
+    //     var partsWithIds = parts.map(async part => {
+    //         const partData = Parts.findOne({
+    //             where: {
+    //                 name: part
+    //             }
+    //         });
+    //         var partplain = partData.get({ plain: true });
+    //         return {
+    //             ticketId: newTicket.id,
+    //             partsId: partplain.id
+    //         }
+    //     });
+    //     await TicketParts.bulkCreate(partsWithIds);
+    //     return res.status(201).render(`/tickets/${newTicket.id}`);
+    // };
+    //     var partsData = await Parts.findOne({
+    //         where: {
+    //             name: req.body.parts
+    //         }
+    //     });
+    //     var part = partsData.get({ plain: true});
+    //     await TicketParts.create({
+    //         ticketId: newTicket.id,
+    //         partsId: parts.id
+    //     })
+        // res.status(201).render(`./tickets/${newTicket.id}`);
+        res.status(201).render('./dashboard');
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
 });
 
-ticket.delete("/:id", async (req, res) => {
+ticket.delete("/:id", isAuth, async (req, res) => {
     try {
         await Ticket.destroy({
             where: {
