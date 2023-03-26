7e2507d2fce6e1c329b17c084aff88f6fa2eeff9
JonG <teflonjuan@icloud.com>
2023-03-26 03:03:58 -0500

    styled login sheet, touched up homepage, linked github buttons

---
+++ public/js/devs.js
@@ -0,0 +1,14 @@
+const jonButton = document.querySelector('#jonButton');
+jonButton.addEventListener('click', () => {
+  window.open('https://github.com/JonathanGuhl', '_blank');
+});
+
+const ameliaButton = document.querySelector('#ameliaButton');
+ameliaButton.addEventListener('click', () => {
+  window.open('https://github.com/AmeliaBigler', '_blank');
+});
+
+const davidButton = document.querySelector('#davidButton');
+davidButton.addEventListener('click', () => {
+  window.open('https://github.com/Cyril1743', '_blank');
+});
