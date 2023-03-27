4cf75ab8f5f0af39a5c66d703512cc31a977988c
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-27 11:14:58 -0500

    add try catch to signup route. add signup js.

---
+++ public/js/signup.js
@@ -0,0 +1,44 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const userType = document.querySelector('#user-type').value;
+    const username = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    
+    if (userType === 'User') {
+        if (username && email && password) {
+            const response = await fetch('/users/signup', {
+                method: 'POST',
+                body: JSON.stringify({ userType, username, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+        
+            if (response.ok) {
+                alert('Sign up successful. Please log in.')
+                document.location.replace('/users/login');
+            } else {
+                alert('Failed to signup');
+            }
+        }
+    } else if (userType === 'Mechanic') {
+        if (username && email && password) {
+            const response = await fetch('/users/signup', {
+                method: 'POST',
+                body: JSON.stringify({ userType, username, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+        
+            if (response.ok) {
+                alert('Sign up successful. Please log in.')
+                document.location.replace('/users/login');
+            } else {
+                alert('Failed to signup');
+            }
+        }
+    }
+    
+  };
+  
+  
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
