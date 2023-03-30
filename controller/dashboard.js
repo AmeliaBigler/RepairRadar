04514fe9968fa4d0ea1310d47a35de1ae96c4535
Amelia Bigler <amaebigler@gmail.com>
2023-03-30 16:04:42 -0500

    add filter for ticket winner in dashboard route. add mechanic dashboard handlebars to display won/open/lost bids.

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -1,13 +1,15 @@
 const dashboard = require("express").Router();
 const { Ticket, User, Bids, Mechanic} = require("../models/index.js");
 const isAuth = require("../util/isAuth");
+const { Op } = require("sequelize");
+const ticket = require("./ticket.js");
 
 dashboard.get("/", isAuth, async (req, res) => {
     try {
         if (req.session.isMechanic) {
             const mechanicData = await Mechanic.findByPk(req.session.user_id, {
-                include: [{ model: Bids }]
-            });
+                include: { model: Bids,
+            }});
             const mechanic = mechanicData.get({ plain: true });
             const bids = mechanic.bids;
             var tickets = bids.map(async bid => {
@@ -19,9 +21,20 @@ dashboard.get("/", isAuth, async (req, res) => {
                 return ticketData.get({ plain: true });
             });
             tickets = await Promise.all(tickets);
+            var openTickets = tickets.filter((i) => {
+                return i.winner === null});
+
+            var wonTickets = tickets.filter((i) => { 
+                return i.winner === req.session.user_id});
+
+            var lostTickets = tickets.filter((i) => {
+                return i.winner !== null && i.winner !== req.session.user_id});
+            
             return res.render("dashboard", {
                 mechanic: mechanic,
-                tickets: tickets,
+                openTickets: openTickets,
+                wonTickets: wonTickets,
+                lostTickets: lostTickets,
                 logged_in: req.session.logged_in,
                 isMechanic: req.session.isMechanic
             });
